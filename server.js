d5be9683b348dcd0b2d4c24445cf86bf079f7cdf
Servio <servio19@gmail.com>
2016-02-21 11:24:16 -0500

    set up server

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
 var express = require('express');
 var app = express();
+var PORT = 3000;
+
+//bodyParser
+var bodyParser = require('bodyParser');
+app.use(bodyParser.urlencoded({ extended: false}));
+
+//database
+var Sequelize = require('sequelize');
+var connection = new Sequelize('', 'root');
+
+//handlebars setup
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+//routes
+
